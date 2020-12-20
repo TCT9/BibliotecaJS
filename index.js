8d1f8aa66f02c37e5e3a07759034715a3bf4f7ce
tct9 <super.adm.lucius@gmail.com>
2020-12-20 19:32:53 -0300

    Atualizado

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
-function olaMundo(){
-    console.log("Olá mundo das bibliotecas JS!!!");
+//retorna um objeto cuja função é contar.
+//Tem os métodos (funções) para retornar o valor do contador (get),
+//alterar o valor do contator (set) e incrementar o valor do contador em uma unidade (incremente)
+let new_contador = function() {
+    
+    //variável privada
+    let i = 0;
+
+    //variáveis publicas
+    return {
+        get: () =>{
+            return i;
+        },
+
+        set: (valor) => {
+            i = valor;
+        },
+
+        incremente: () => {
+            i++;
+        }
+
+    };
+
 }
 
-module.exports = olaMundo;
+// expõem a função paar ser usada como um módulo
+module.exports = new_contador;
